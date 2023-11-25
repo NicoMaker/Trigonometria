fc68abac3d9938fb4f767dbdbcd473dd72aa5300
Nico Maker <nicola.marano02@gmail.com>
2023-11-25 11:19:24 +0100

    COdice Per Fare calcoli trigonometrici

---
+++ Trigonometria.js
@@ -0,0 +1,70 @@
+function CalcolaTrigonometria() {
+    let gradi = Inserisci();
+    let info = "", stampaHTML = "";
+    let options = document.getElementById("options").value;
+
+    if (isNaN(gradi)) {
+        stampaHTML = "inserisci un numero valido";
+        document.getElementById("risultato").innerHTML = stampaHTML;
+    }
+    else {
+        if (options === "Rad") {
+            info = "Radianti";
+
+            Risultato(info, gradi);
+
+        } else if (options === "Grad") {
+            info = "Gradi";
+            gradi = Radianti(gradi);
+
+            Risultato(info, gradi);
+        }
+    }
+}
+
+function Inserisci() {
+    let gradi = parseInt(document.getElementById("gradi-radianti").value);
+    return gradi;
+}
+
+function Radianti(gradi) {
+    let gradorad = (gradi * Math.PI) / 180;
+    return gradorad;
+}
+
+function Gradi(gradi) {
+    let gradograd = (gradi * 180) / Math.PI;
+    return gradograd;
+}
+
+function Risultato(info, gradi) {
+    let stampaHTML = "", cos = 0, sen = 0, tan = 0, arcsen = 0, arccos = 0, arctan = 0;
+
+    sen = Math.sin(gradi);
+    cos = Math.cos(gradi);
+    tan = Math.tan(gradi);
+    arcsen = Math.asin(gradi);
+    arccos = Math.acos(gradi);
+    arctan = Math.atan(gradi);
+
+    if (info == "Gradi")
+        gradi = Gradi(gradi);
+
+    stampaHTML += "Dati del calcolo di " + gradi + " " + info;
+    stampaHTML += "<br>";
+    stampaHTML += "<br>";
+    stampaHTML += "Seno : " + sen;
+    stampaHTML += "<br>";
+    stampaHTML += "Coseno : " + cos;
+    stampaHTML += "<br>";
+    stampaHTML += "Tangente : " + tan;
+    stampaHTML += "<br>";
+    stampaHTML += "Arcoseno : " + arcsen;
+    stampaHTML += "<br>";
+    stampaHTML += "Arcocoseno : " + arccos;
+    stampaHTML += "<br>";
+    stampaHTML += "Arcotangente : " + arctan;
+    stampaHTML += "<br>";
+
+    document.getElementById("risultato").innerHTML = stampaHTML;
+}
